86b540d0c29b37fc8ae20a03260640778c665fa2
Lubos Lenco <lubos.lenco@gmail.com>
2017-06-15 13:24:15 +0200

    Sync

---
+++ build/make.js
@@ -0,0 +1,13 @@
+let fs = require('fs');
+
+let writeHtml = function(fileName, title) {
+	let header = fs.readFileSync('header.html', 'utf8').replace('ID_TITLE', title);
+	let footer = fs.readFileSync('footer.html', 'utf8');
+	let html = fs.readFileSync(fileName, 'utf8');
+	fs.writeFileSync('../' + fileName, header + html + footer);
+}
+
+writeHtml('index.html', 'ArmorPaint');
+writeHtml('news.html', 'News');
+writeHtml('community.html', 'Community');
+writeHtml('download.html', 'Download');
