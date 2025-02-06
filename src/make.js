2b23d6b6622e6c537e846911d3f9937993605394
luboslenco <lubos.lenco@gmail.com>
2025-02-06 15:47:49 +0100

    Sync

--- src/make.js
+++ src/make.js
@@ -22,51 +22,52 @@ writeHtml('index.html');
 writeHtml('news.html');
 writeHtml('community.html');
 writeHtml('download.html');
+writeHtml('login.html');
 writeHtml('notes.html');
 writeHtml('howto.html');
 writeHtml('privacy.html');
 writeManual('manual.html', 'manual_header.html', 'manual_footer.html', '../manual.md');
 
 // cloud.html
-{
-	fs.rmSync('../img/cloud', { recursive: true });
-	fs.mkdirSync('../img/cloud');
-	let cloud_grid = `
-		<div style="justify-content: center; display: grid; grid-template-columns: repeat(auto-fill, 170px); max-width: 900px; margin: auto;">
-	`;
-	let icon_folders = [
-		'../../armorpaint_cloud/public/cloud/materials/procedural',
-		'../../armorpaint_cloud/public/cloud/materials/armorlab',
-		'../../armorpaint_cloud/public/cloud/materials/polyhaven',
-		'../../armorpaint_cloud/public/cloud/materials/ambientcg/approximated',
-		'../../armorpaint_cloud/public/cloud/materials/ambientcg/multiangle',
-		'../../armorpaint_cloud/public/cloud/materials/ambientcg/photogrammetry',
-		'../../armorpaint_cloud/public/cloud/materials/ambientcg/procedural',
-		'../../armorpaint_cloud/public/cloud/decals',
-		'../../armorpaint_cloud/public/cloud/decals/ambientcg',
-		'../../armorpaint_cloud/public/cloud/brushes',
-		'../../armorpaint_cloud/public/cloud/hdri/ambientcg',
-		'../../armorpaint_cloud/public/cloud/hdri/polyhaven',
-		'../../armorpaint_cloud/public/cloud/meshes'
-	];
-	for (let folder of icon_folders) {
-		fs.readdirSync(folder).forEach(file => {
-			let isMaterial = folder.indexOf('/materials') > 0;
-			if (file.endsWith(isMaterial ? '.png' : '.jpg')) {
-				fs.copyFileSync(folder + '/' + file, '../img/cloud/' + file);
-				label = file.slice(0, -9);
-				if (label.length > 13) label = label.substring(0, 11) + '...';
-				let filePath = 'img/cloud/' + file;
-				if (isMaterial && fs.existsSync('../img/cloud_raytraced/' + file)) {
-					filePath = 'img/cloud_raytraced/' + file;
-				}
-				cloud_grid += '<div style="width: 70%; text-align: center; margin: auto;"><img style="width: 128px;" src="' + filePath + '"/><br>' + label + '<br><br></div>';
-			}
-		});
-	}
-	cloud_grid += '</div>';
-	writeHtml('cloud.html', cloud_grid);
-}
+// {
+// 	fs.rmSync('../img/cloud', { recursive: true });
+// 	fs.mkdirSync('../img/cloud');
+// 	let cloud_grid = `
+// 		<div style="justify-content: center; display: grid; grid-template-columns: repeat(auto-fill, 170px); max-width: 900px; margin: auto;">
+// 	`;
+// 	let icon_folders = [
+// 		'../../armorpaint_cloud/public/cloud/materials/procedural',
+// 		'../../armorpaint_cloud/public/cloud/materials/armorlab',
+// 		'../../armorpaint_cloud/public/cloud/materials/polyhaven',
+// 		'../../armorpaint_cloud/public/cloud/materials/ambientcg/approximated',
+// 		'../../armorpaint_cloud/public/cloud/materials/ambientcg/multiangle',
+// 		'../../armorpaint_cloud/public/cloud/materials/ambientcg/photogrammetry',
+// 		'../../armorpaint_cloud/public/cloud/materials/ambientcg/procedural',
+// 		'../../armorpaint_cloud/public/cloud/decals',
+// 		'../../armorpaint_cloud/public/cloud/decals/ambientcg',
+// 		'../../armorpaint_cloud/public/cloud/brushes',
+// 		'../../armorpaint_cloud/public/cloud/hdri/ambientcg',
+// 		'../../armorpaint_cloud/public/cloud/hdri/polyhaven',
+// 		'../../armorpaint_cloud/public/cloud/meshes'
+// 	];
+// 	for (let folder of icon_folders) {
+// 		fs.readdirSync(folder).forEach(file => {
+// 			let isMaterial = folder.indexOf('/materials') > 0;
+// 			if (file.endsWith(isMaterial ? '.png' : '.jpg')) {
+// 				fs.copyFileSync(folder + '/' + file, '../img/cloud/' + file);
+// 				label = file.slice(0, -9);
+// 				if (label.length > 13) label = label.substring(0, 11) + '...';
+// 				let filePath = 'img/cloud/' + file;
+// 				if (isMaterial && fs.existsSync('../img/cloud_raytraced/' + file)) {
+// 					filePath = 'img/cloud_raytraced/' + file;
+// 				}
+// 				cloud_grid += '<div style="width: 70%; text-align: center; margin: auto;"><img style="width: 128px;" src="' + filePath + '"/><br>' + label + '<br><br></div>';
+// 			}
+// 		});
+// 	}
+// 	cloud_grid += '</div>';
+// 	writeHtml('cloud.html', cloud_grid);
+// }
 
 // rss.xml
 {
