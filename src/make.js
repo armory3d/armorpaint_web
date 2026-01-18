c6d56bc891b9130fb9d222eff3d7e1fa613ee06b
luboslenco <lubos.lenco@gmail.com>
2026-01-18 15:03:55 +0100

    Sync

--- src/make.js
+++ src/make.js
@@ -1,4 +1,5 @@
 let fs = require('fs');
+const https = require('https');
 let header = fs.readFileSync('header.html', 'utf8');
 let cover = fs.readFileSync('cover.html', 'utf8');
 let footer = fs.readFileSync('footer.html', 'utf8');
@@ -32,41 +33,70 @@ writeManual('manual.html', 'manual_header.html', 'manual_footer.html', '../manua
 
 // cloud.html
 {
-	fs.rmSync('../img/cloud', { recursive: true });
-	fs.mkdirSync('../img/cloud');
-	let cloud_grid = `
+	function download(url, dest) {
+		return new Promise((resolve, reject) => {
+			const file = fs.createWriteStream(dest);
+			https.get(url, response => {
+				response.pipe(file);
+				file.on('finish', () => {
+					file.close();
+					resolve();
+				});
+			}).on('error', err => {
+				fs.unlink(dest);
+				reject(err);
+			});
+		});
+	}
+
+	(async () => {
+		// fs.rmSync('../img/cloud', { recursive: true, force: true });
+		// fs.mkdirSync('../img/cloud');
+
+		await download('https://cloud.armory3d.com/index.txt', 'index.txt');
+		const content = fs.readFileSync('index.txt', 'utf8');
+		const lines = content.split('\n');
+
+		let icon_files = [];
+		let pathToMaterialMap = new Map();
+
+		for (let line of lines) {
+			if (line.trim() === '') continue;
+			const parts = line.split(' ');
+			const path = parts[0];
+			if (path.endsWith('_icon.jpg') || path.endsWith('_icon.png')) {
+			icon_files.push(path);
+			const file = path.split('/').pop();
+			const isMaterial = path.indexOf('/materials') > 0;
+			pathToMaterialMap.set(file, isMaterial);
+			}
+		}
+
+		// for (let icon_file of icon_files) {
+		// 	const url = 'https://cloud.armory3d.com/' + icon_file;
+		// 	const file = icon_file.split('/').pop();
+		// 	await download(url, '../img/cloud/' + file);
+		// }
+
+		let cloud_grid = `
 		<div style="justify-content: center; display: grid; grid-template-columns: repeat(auto-fill, 170px); max-width: 900px; margin: auto;">
-	`;
-	let icon_folders = [
-		'../../armorpaint_cloud/cloud/materials/procedural',
-		'../../armorpaint_cloud/cloud/materials/armorlab',
-		'../../armorpaint_cloud/cloud/materials/polyhaven',
-		'../../armorpaint_cloud/cloud/materials/ambientcg/approximated',
-		'../../armorpaint_cloud/cloud/materials/ambientcg/multiangle',
-		'../../armorpaint_cloud/cloud/materials/ambientcg/photogrammetry',
-		'../../armorpaint_cloud/cloud/materials/ambientcg/procedural',
-		'../../armorpaint_cloud/cloud/decals',
-		'../../armorpaint_cloud/cloud/decals/ambientcg',
-		'../../armorpaint_cloud/cloud/brushes',
-		'../../armorpaint_cloud/cloud/hdri/ambientcg',
-		'../../armorpaint_cloud/cloud/hdri/polyhaven',
-		'../../armorpaint_cloud/cloud/meshes'
-	];
-	for (let folder of icon_folders) {
-		fs.readdirSync(folder).forEach(file => {
-			let isMaterial = folder.indexOf('/materials') > 0;
-			if (file.endsWith(isMaterial ? '.png' : '.jpg')) {
-				fs.copyFileSync(folder + '/' + file, '../img/cloud/' + file);
-				label = file.slice(0, -9);
+		`;
+
+		fs.readdirSync('../img/cloud').sort().forEach(file => {
+			const isIconJpg = file.endsWith('_icon.jpg');
+			const isIconPng = file.endsWith('_icon.png');
+			const isMaterial = pathToMaterialMap.get(file);
+			if ((isMaterial && isIconPng) || (!isMaterial && isIconJpg)) {
+				let label = file.slice(0, -9);
 				if (label.length > 13) label = label.substring(0, 11) + '...';
 				let filePath = 'img/cloud/' + file;
-				if (isMaterial && fs.existsSync('../img/cloud_raytraced/' + file)) {
-					filePath = 'img/cloud_raytraced/' + file;
-				}
 				cloud_grid += '<div style="width: 70%; text-align: center; margin: auto;"><img style="width: 128px;" src="' + filePath + '"/><br>' + label + '<br><br></div>';
 			}
 		});
-	}
-	cloud_grid += '</div>';
-	writeHtml('cloud.html', cloud_grid);
+
+		cloud_grid += '</div>';
+		writeHtml('cloud.html', cloud_grid);
+
+		fs.unlinkSync('index.txt');
+	})();
 }
